0220a4f2efd5708fe9b607b8693cdf44d3f4e726
William Qian <wqian94@mit.edu>
2014-04-11 12:36:55 -0400

    Starting code.

---
+++ src/model/JottoModel.java
@@ -0,0 +1,14 @@
+package model;
+
+/**
+ * TODO Write the specification for JottoModel
+ */
+public class JottoModel {
+    
+    /**
+     * TODO Write the specification for makeGuess
+     */
+    public void makeGuess(String guess) {
+        // TODO Problem 1
+    }
+}
